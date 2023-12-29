3fa4821c435dde99a1a2bd60e11025d6b8c041e6
SnowFlake <57414251+wain-aina@users.noreply.github.com>
2023-12-29 14:02:58 +0300

    Add files via upload

---
+++ public/js/calendar.js
@@ -0,0 +1,44 @@
+
+// Initialize evo-calendar in your script file or an inline <script> tag
+// $(document).ready(function() {
+//     $('#calendar').evoCalendar({
+//         settingName: "hello",
+//         todayHighlight:true,
+//         format:'yyyy/mm/dd',
+//         calendarEvents: myEvents,
+//     })
+// })
+//
+// myEvents = fetch('/adminDash/calendar')
+//     .then(response => response.json())
+//     .then(diary => {
+//         JSON.parse(diary)
+//     });
+
+// myEvents = [
+//     {
+//         id:"required-id-1",
+//         name:"New Year",
+//         date:"Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
+//         type:"holiday",
+//         everyYear:true
+//     },
+//     {
+//         id:"required-id-2",
+//         name:"Valentine's Day",
+//         date:"Fri Feb 14 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
+//         type:"holiday",
+//         everyYear:true,
+//         color:"#222"
+//
+//     },
+//     {
+//         id:"required-id-3",
+//         name:"Custom Date",
+//         badge:"08/03 - 08/05",
+//         date: ["August/03/2020","August/05/2020"],
+//         description:"Description here",
+//         type:"event",
+//     },
+// ]
+
